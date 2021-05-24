a5c35d3165a8af2a25343bb3be9e040108f50ded
Nicole Martin <n.martinpdx@gmail.com>
2021-05-24 14:28:36 -0700

    add buttons to homepage

--- bookings-web/src/components/ui/Header.jsx
+++ bookings-web/src/components/ui/Header.jsx
@@ -5,6 +5,7 @@ export default class Header extends Component {
   render() {
     return (
       <div>
+        <NavLink to="/">Home</NavLink>
         <NavLink to="/signup">Sign Up</NavLink>
         <NavLink to="/login">Log In</NavLink>
         <NavLink to="/getaways">Places</NavLink>
--- bookings-web/src/components/ui/HomePage.jsx
+++ bookings-web/src/components/ui/HomePage.jsx
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-export class LoginPage extends Component {
+export class HomePage extends Component {
   render() {
     return (
-      <div>
-        <h1>This is Home Page</h1>
-      </div>
+      <>
+        <h1>Welcome to Alchemy Getaways!</h1>
+        <div className="btn-box">
+          <h2>
+            <Link to="/signup" className="links">
+              <button>New User</button>
+            </Link>
+          </h2>
+          <h2>
+            <Link to="/login" className="links">
+              <button>Returning User</button>
+            </Link>
+          </h2>
+        </div>
+      </>
     );
   }
 }
 
-export default LoginPage;
+export default HomePage;
